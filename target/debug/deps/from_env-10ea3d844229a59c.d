7388d580d5e69aa735809d5b7ba2c106b4ba880e
wangjun <444055828@qq.com>
2023-09-23 23:59:43 +0800

    ignore target directory

--- target/debug/deps/from_env-10ea3d844229a59c.d
+++
@@ -1,9 +0,0 @@
-/home/wangwenqing/hobby/rust/from-env/target/debug/deps/from_env-10ea3d844229a59c.rmeta: src/lib.rs Cargo.toml
-
-/home/wangwenqing/hobby/rust/from-env/target/debug/deps/from_env-10ea3d844229a59c.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
